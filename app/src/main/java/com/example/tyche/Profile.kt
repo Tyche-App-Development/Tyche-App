48326d43008cfaf92ae0f972eb0dd18a3fa4bcff
Rui Carvalho <rui.carvalho@ipvc.pt>
2025-04-09 10:05:18 +0100

    Fix: Adding logout

--- app/src/main/java/com/example/tyche/Profile.kt
+++ app/src/main/java/com/example/tyche/Profile.kt
@@ -1,6 +1,8 @@
 package com.example.tyche
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,28 @@ class Profile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val btnLogout = findViewById<Button>(R.id.logoutButton)
+        btnLogout.setOnClickListener {
+            logoutUser()
+        }
+    }
+
+    private fun logoutUser() {
+        // Remove token
+        val sharedPref = getSharedPreferences("auth", MODE_PRIVATE)
+        sharedPref.edit().remove("token").apply()
+
+        //Redirect to Login Page
+        val intent = Intent(this, Login::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
